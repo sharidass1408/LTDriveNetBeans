67c527067d2cf5c02bb3f7a97338a1dac4bdd66d
sai <sharidass1408@gmail.com>
2019-03-04 12:46:09 -0500

    Changes to add VehicleProgram to controller

--- src/javafxltdrive/FXMLDocumentController.java
+++ src/javafxltdrive/FXMLDocumentController.java
@@ -7,6 +7,9 @@ package javafxltdrive;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -14,6 +17,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.Button;
 import javafx.stage.FileChooser;
 import java.io.File;
+import javafx.scene.control.ChoiceBox;
 
 /**
  *
@@ -24,14 +28,17 @@ public class FXMLDocumentController implements Initializable {
     @FXML
     private Button browseCatalog;
     
-    
-    
+    @FXML
+    private ChoiceBox vehicleProgram = new ChoiceBox<>();
+
+
     public void handleBrowseCatalogClick(){
         FileChooser fc = new FileChooser();
         File selectedFile = fc.showOpenDialog(null);
         
         if(selectedFile != null){
             System.out.println("Valid file");
+
         }else{
             System.out.println("File not valid");
         }
@@ -42,7 +49,20 @@ public class FXMLDocumentController implements Initializable {
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
-    
+        constVehicleProgramDropDown();
+    }
+
+    private void constVehicleProgramDropDown() {
+        vehicleProgram.getItems().add("- Select a Vehicle Program - ");
+        vehicleProgram.getItems().add("2005 P131");
+        vehicleProgram.getItems().add("2007 P356");
+        vehicleProgram.getItems().add("2009 T1");
+        vehicleProgram.getItems().add("E-series");
+        vehicleProgram.getItems().add("P221 Prod");
+        vehicleProgram.getItems().add("Reference");
+        vehicleProgram.getItems().add("SUV");
+        vehicleProgram.getItems().add("TRANSIT");
+        vehicleProgram.setValue("- Select a Vehicle Program - ");
+    }
+
 }
